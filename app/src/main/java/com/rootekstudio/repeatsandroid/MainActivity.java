33397c4d43c61600b911bccedeca4ff6afbe59d7
Jakub Sieradzki <jakub_sieradzki@outlook.com>
2018-11-11 19:50:07 +0100

    2.0_alpha06

--- app/src/main/java/com/rootekstudio/repeatsandroid/MainActivity.java
+++ app/src/main/java/com/rootekstudio/repeatsandroid/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -23,14 +29,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         DatabaseHelper DB = new DatabaseHelper(this);
 
         final Intent intent = new Intent(this, RepeatsAddEditActivity.class);
-
-        final ListView listView = findViewById(R.id.AllSetsList);
-
+        final LinearLayout linear = findViewById(R.id.mainLinear);
+        final LayoutInflater inflater = LayoutInflater.from(this);
         final List<RepeatsListDB> ALL  = DB.AllItemsLIST();
         int ItemsCounts = ALL.size();
+
+        int a = 0;
+        for(int i = 0; i < ItemsCounts; i++)
+        {
+            inflater.inflate(R.layout.mainactivitylistitem, linear);
+            View view = linear.getChildAt(i);
+            Button but = view.findViewById(R.id.TempButton);
+            but.setText(ALL.get(i).getTableName());
+            but.setTag(ALL.get(i).getitle());
+            but.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Button btn = (Button) v;
+                    String TITLE = v.getTag().toString();
+                    String TABLE_NAME = btn.getText().toString();
+                    intent.putExtra("ISEDIT", TITLE);
+                    intent.putExtra("NAME", TABLE_NAME);
+                    startActivity(intent);
+                }
+            });
+        }
 
         ArrayList<String> List = new ArrayList<String>();
 
@@ -42,22 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, List);
 
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                String TITLE = ALL.get(position).getitle();
-                String TABLE_NAME = ALL.get(position).getTableName();
-                intent.putExtra("ISEDIT", TITLE);
-                intent.putExtra("NAME", TABLE_NAME);
-                startActivity(intent);
-            }
-        });
-
-        final Button btn = findViewById(R.id.button);
+        final FloatingActionButton btn = findViewById(R.id.fab);
         btn.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View view)
