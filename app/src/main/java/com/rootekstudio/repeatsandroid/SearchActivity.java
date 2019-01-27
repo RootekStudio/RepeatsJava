0ba7a1a5e1ece93c5d69ec50865a52869de776c1
Jakub Sieradzki <jakub_sieradzki@outlook.com>
2019-01-27 14:16:55 +0100

    2.0_alpha19

--- app/src/main/java/com/rootekstudio/repeatsandroid/SearchActivity.java
+++ app/src/main/java/com/rootekstudio/repeatsandroid/SearchActivity.java
@@ -3,13 +3,18 @@ package com.rootekstudio.repeatsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Vector;
 
 class SearchItem
 {
@@ -17,6 +22,7 @@ class SearchItem
     String Answer;
     String Title;
     String TableName;
+    String AllItem;
 
     public SearchItem(String Question, String Answer, String Title, String TableName)
     {
@@ -24,34 +30,71 @@ class SearchItem
         this.Answer = Answer;
         this.Title = Title;
         this.TableName = TableName;
+        this.AllItem = Question + System.getProperty("line.separator") + Answer + System.getProperty("line.separator") + Title;
     }
 
-    String getQuestion() { return Title; }
-    String getAnswer() { return Answer; }
+    String gQuestion() { return Question; }
+    String gAnswer() { return Answer; }
+    String gTitle() { return Title; }
+    String gTableName() {return TableName; }
+    String gItem() {return AllItem; }
 
+    public void sQuestion(String Question) { this.Question = Question; }
+    public void sAnswer(String Answer) {this.Answer = Answer; }
+    public void sTitle(String Title) {this.Title = Title; }
+    public void sTableName(String TableName) {this.TableName = TableName;}
 }
 
-public class SearchActivity extends AppCompatActivity
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener
 {
+    SearchAdapter adapter;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        List<SearchItem>sItem = new ArrayList<>();
+        DatabaseHelper DB = new DatabaseHelper(this);
+        List<SearchItem> sItem = new ArrayList<>();
+
+        List<RepeatsListDB> list = DB.AllItemsLIST();
+        int count = list.size();
+
+        for (int i = 0; i < count; i++) {
+            RepeatsListDB singleitem = list.get(i);
+            String TableName = singleitem.TableName;
+            String title = singleitem.Title;
+
+            List<RepeatsSingleSetDB> single = DB.AllItemsSET(TableName);
+
+            int singlecount = single.size();
+            for (int j = 0; j < singlecount; j++) {
+                RepeatsSingleSetDB singleSetDB = single.get(j);
+                String Q = singleSetDB.getQuestion();
+                String A = singleSetDB.getAnswer();
+
+                sItem.add(new SearchItem(Q, A, title, TableName));
+            }
+        }
+
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(this);
+        ListView listView = findViewById(R.id.searchList);
+        adapter = new SearchAdapter(this, sItem);
+        listView.setAdapter(adapter);
+    }
 
 
-        SearchItem item = new SearchItem("Q", "A", "T", "A");
-        sItem.add(item);
+    @Override
+    public boolean onQueryTextSubmit(String query)
+    {
+        return false;
+    }
 
-        Boolean ABC = sItem.contains(item);
-        int a = 0;
-
-//        HashMap QandA = new HashMap<String, String>();
-//        QandA.put("A", "A");
-//        QandA.
-
-
+    @Override
+    public boolean onQueryTextChange(String newText)
+    {
+        adapter.search(newText);
+        return false;
     }
 }
---
+++ app/src/main/java/com/rootekstudio/repeatsandroid/SearchAdapter.java
@@ -0,0 +1,86 @@
+package com.rootekstudio.repeatsandroid;
+
+import android.content.Context;
+import android.graphics.Picture;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class SearchAdapter extends BaseAdapter
+{
+    Context context;
+    LayoutInflater inflater;
+    List<SearchItem> items;
+    ArrayList<SearchItem> arrayItems;
+
+    public SearchAdapter(Context context, List<SearchItem> items)
+    {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.items = items;
+        this.arrayItems = new ArrayList<>();
+        this.arrayItems.addAll(items);
+    }
+
+    @Override
+    public int getCount()
+    {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+            convertView = inflater.inflate(R.layout.search_item, null);
+            TextView t1 = convertView.findViewById(R.id.SearchQ);
+            TextView t2 = convertView.findViewById(R.id.SearchA);
+            TextView t3 =  convertView.findViewById(R.id.SearchS);
+
+            SearchItem item = items.get(position);
+            t1.setText(item.gQuestion());
+            t2.setText(item.gAnswer());
+            t3.setText(item.gTitle());
+
+        return convertView;
+    }
+
+    public void search(String text)
+    {
+        text = text.toLowerCase(Locale.getDefault());
+        items.clear();
+
+        if(text.length() == 0)
+        {
+            items.addAll(arrayItems);
+        }
+        else
+        {
+            for(SearchItem x : arrayItems)
+            {
+                if(x.gItem().toLowerCase(Locale.getDefault()).contains(text))
+                {
+                    items.add(x);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+}
