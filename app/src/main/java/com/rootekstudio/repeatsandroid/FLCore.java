a528a1bc066055f2cc3a8ab116fd18cb9ef48014
RootekStudio <rootekstudio@outlook.com>
2019-10-12 23:44:23 +0200

    Fast Learning v.1

--- app/src/main/java/com/rootekstudio/repeatsandroid/FLCore.java
+++ app/src/main/java/com/rootekstudio/repeatsandroid/FLCore.java
@@ -1,17 +1,72 @@
 package com.rootekstudio.repeatsandroid;
 
-public class FLCore {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-    static public void core(String answer, SingleFLitem FLitem) {
-        if(CheckAnswer.isAnswerCorrect(answer, FLitem.getCorrectAnswer(), "false")) {
-            int correct = FLitem.getCorrectAnswersInt();
-            correct++;
-            FLitem.setCorrectAnswersInt(correct);
+public class FLCore {
+    static int answered;
+    int correctAnswers;
+
+    static List<RepeatsSingleSetDB> selectedItems;
+
+    public Boolean done = false;
+    int allSetItemsSize;
+
+    public void start(List<RepeatsSingleSetDB> allSetItems) {
+        selectedItems = new ArrayList<>();
+        allSetItemsSize = allSetItems.size();
+        answered = 0;
+        correctAnswers = 0;
+
+        if(allSetItemsSize < 10) {
+            for (int i = 0; i < allSetItemsSize; i++) {
+                selectedItems.add(allSetItems.get(i));
+            }
         }
         else {
-            int wrong = FLitem.getWrongAnswersInt();
-            wrong++;
-            FLitem.setWrongAnswersInt(wrong);
+            for (int i = 0; i < 10; i++) {
+                Random random = new Random();
+                int index = random.nextInt((allSetItemsSize-1));
+                selectedItems.add(allSetItems.get(index));
+            }
         }
+    }
+
+    public boolean checkAnswered(String answer) {
+        if (CheckAnswer.isAnswerCorrect(answer, selectedItems.get(answered).getAnswer(), "false")) {
+            correctAnswers++;
+
+            if(allSetItemsSize < 10) {
+                if(correctAnswers == allSetItemsSize) {
+                    done = true;
+                }
+            }
+            else {
+                if(correctAnswers == 10) {
+                    done = true;
+                }
+            }
+
+            answered++;
+            return true;
+
+        } else {
+            int selectedLastIndex = selectedItems.size()-1;
+            Random random = new Random();
+            int index = random.nextInt(selectedLastIndex - (answered+1)) + (answered+1);
+            selectedItems.add(selectedItems.get(index));
+            selectedItems.set(index, selectedItems.get(answered));
+
+            answered++;
+            return false;
+        }
+    }
+
+    public static String[] gQuestionAndAnswer()
+    {
+        RepeatsSingleSetDB single = selectedItems.get(answered);
+        String[] qanda = {single.getQuestion(), single.getAnswer(), single.getImag()};
+        return qanda;
     }
 }
--- app/src/main/java/com/rootekstudio/repeatsandroid/activities/FastLearningActivity.java
+++ app/src/main/java/com/rootekstudio/repeatsandroid/activities/FastLearningActivity.java
@@ -1,99 +1,184 @@
 package com.rootekstudio.repeatsandroid.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.rootekstudio.repeatsandroid.CheckAnswer;
 import com.rootekstudio.repeatsandroid.FLCore;
 import com.rootekstudio.repeatsandroid.R;
+import com.rootekstudio.repeatsandroid.RepeatsHelper;
 import com.rootekstudio.repeatsandroid.RepeatsSingleSetDB;
 import com.rootekstudio.repeatsandroid.SingleFLitem;
 import com.rootekstudio.repeatsandroid.database.DatabaseHelper;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FastLearningActivity extends AppCompatActivity {
-
-    List<RepeatsSingleSetDB> itemsSetList;
-    List<SingleFLitem> FLitems;
-
-    TextView txt;
+    TextView question;
+    TextView correctA;
     EditText editText;
-    Button button;
+    static Button button;
+    ImageView badge;
+    ImageView image;
+    LinearLayout linearQbox;
 
-    int itemCount = 0;
+    FLCore FL;
+
+    Context context;
+
+    String setID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        boolean isDark = RepeatsHelper.DarkTheme(this, false);
+
         setContentView(R.layout.fast_learning);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        context = this;
+
+        FL = new FLCore();
 
         Intent intent = getIntent();
 
-        String setID = intent.getStringExtra("TableName");
+        setID = intent.getStringExtra("TableName");
 
         DatabaseHelper DB = new DatabaseHelper(this);
-        itemsSetList = DB.AllItemsSET(setID);
-        FLitems = new ArrayList<>();
+        List<RepeatsSingleSetDB> itemsSetList = DB.AllItemsSET(setID);
 
-        txt = findViewById(R.id.textViewFL);
+        FL.start(itemsSetList);
+
+        question = findViewById(R.id.questionFL);
+        correctA = findViewById(R.id.correctAnswerFL);
         editText = findViewById(R.id.editTextFL);
         button = findViewById(R.id.buttonFL);
+        badge = findViewById(R.id.badgeFL);
+        image = findViewById(R.id.imageViewFL);
+        linearQbox = findViewById(R.id.linearQuestionBox);
 
-        int setItemsCount = itemsSetList.size();
-        setItemsCount-=10;
-
-        for(int i = 0; i < 3; i++) {
-            RepeatsSingleSetDB singleSet = itemsSetList.get(i);
-            String question = singleSet.getQuestion();
-            String answer = singleSet.getAnswer();
-
-            FLitems.add(new SingleFLitem(question, answer, 0,0));
+        if(!isDark) {
+            linearQbox.setBackgroundResource(R.drawable.layout_mainshape);
+            editText.setBackgroundResource(R.drawable.edittext_shape);
         }
 
         loadQuestion();
     }
 
     void loadQuestion() {
-        final SingleFLitem FLitem = FLitems.get(itemCount);
-        txt.setText(FLitem.FLquestion);
+        if(!FL.done) {
+            //reset
+            editText.setText("");
+            correctA.setText("");
+            correctA.setVisibility(View.GONE);
+            badge.setVisibility(View.GONE);
+            editText.setEnabled(true);
+            image.setImageBitmap(null);
+            image.setVisibility(View.GONE);
+
+            final String[] qanda = FLCore.gQuestionAndAnswer();
+            question.setText(qanda[0]);
+
+            String img = qanda[2];
+            if(!img.equals("")) {
+                image.setVisibility(View.VISIBLE);
+
+                File file = new File(getFilesDir(), img);
+                FileInputStream inputStream = null;
+                try {
+                    inputStream = new FileInputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+
+                image.setImageBitmap(bitmap);
+            }
+
+            button.setText(R.string.Check);
+            button.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_box, 0, 0, 0);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String answer = editText.getText().toString();
+                    editText.setEnabled(false);
+
+                    Button button = (Button) view;
+                    button.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_navigate_next, 0, 0, 0);
+                    button.setText(R.string.Next);
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            loadQuestion();
+                        }
+                    });
+
+                    badge.setVisibility(View.VISIBLE);
+                    correctA.setVisibility(View.VISIBLE);
+                    if (!FL.checkAnswered(answer)) {
+                        String notCorrect = getString(R.string.IncorrectAnswer1) + ". " + getString(R.string.IncorrectAnswer2) + ": " + qanda[1];
+                        badge.setImageResource(R.drawable.ic_clear);
+                        badge.setColorFilter(ContextCompat.getColor(context, android.R.color.holo_red_dark));
+                        correctA.setText(notCorrect);
+                    } else {
+                        correctA.setText(R.string.CorrectAnswer2);
+                        badge.setImageResource(R.drawable.ic_check);
+                        badge.setColorFilter(ContextCompat.getColor(context, android.R.color.holo_green_light));
+                    }
+                }
+            });
+        }
+        else {
+            end();
+        }
+    }
+    void end() {
+        linearQbox.setVisibility(View.INVISIBLE);
+        correctA.setText(R.string.FLendText);
+        button.setText(R.string.OnceAgain);
+        badge.setVisibility(View.INVISIBLE);
+        button.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_refresh, 0, 0, 0);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String answer = editText.getText().toString();
-
-                if(CheckAnswer.isAnswerCorrect(answer, FLitem.getCorrectAnswer(), "false")) {
-                    int correct = FLitem.getCorrectAnswersInt();
-                    correct++;
-                    FLitem.setCorrectAnswersInt(correct);
-                }
-                else {
-                    int wrong = FLitem.getWrongAnswersInt();
-                    wrong++;
-                    FLitem.setWrongAnswersInt(wrong);
-
-                }
-
-                Button button = (Button)view;
-                button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        loadQuestion();
-                    }
-                });
+                finish();
+                Intent intent = new Intent(context, FastLearningActivity.class);
+                intent.putExtra("TableName", setID);
+                startActivity(intent);
             }
         });
+    }
 
-        itemCount++;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == android.R.id.home)
+        {
+            onBackPressed();
+        }
+        return true;
     }
 
 }
